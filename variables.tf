0e7d8c4afcd1944e627a2a171beae59c10828346
Nick Charlton <nick@nickcharlton.net>
2015-06-22 15:20:35 +0100

    Adds a default set of variables.

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "region" {
+    description = "EC2 Region for the VPC"
+    default = "eu-west-1"
+}
+
+variable "amis" {
+    description = "AMIs by region"
+    default = {
+        eu-west-1 = "ami-f1810f86" # ubuntu 14.04 LTS
+    }
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the whole VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "CIDR for the Private Subnet"
+    default = "10.0.1.0/24"
+}
