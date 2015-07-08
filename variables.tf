caef4ccccb34b36d7ed1807e1ea255d60b2a5525
Nick Charlton <nick@nickcharlton.net>
2015-07-08 15:38:06 +0100

    Reworks the default variables.

--- variables.tf
+++ variables.tf
@@ -1,8 +1,9 @@
-variable "access_key" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_key_path" {}
+variable "aws_key_name" {}
 
-variable "secret_key" {}
-
-variable "region" {
+variable "aws_region" {
     description = "EC2 Region for the VPC"
     default = "eu-west-1"
 }
