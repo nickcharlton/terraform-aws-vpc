56b51565883913f12984cff2d55a15c7d61a510c
Nick Charlton <nick@nickcharlton.net>
2015-07-08 21:55:51 +0100

    Adds the public + private configurations.

---
+++ private.tf
@@ -0,0 +1,66 @@
+/*
+  Database Servers
+*/
+resource "aws_security_group" "db" {
+    name = "vpc_db"
+    description = "Allow incoming database connections."
+
+    ingress { # SQL Server
+        from_port = 1433
+        to_port = 1433
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.web.id}"]
+    }
+    ingress { # MySQL
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.web.id}"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+
+    egress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    tags {
+        Name = "DBServerSG"
+    }
+}
+
+resource "aws_instance" "db-1" {
+    ami = "${lookup(var.amis, var.aws_region)}"
+    availability_zone = "eu-west-1a"
+    instance_type = "m1.small"
+    key_name = "${var.aws_key_name}"
+    security_groups = ["${aws_security_group.db.id}"]
+    subnet_id = "${aws_subnet.eu-west-1a-private.id}"
+    source_dest_check = false
+
+    tags {
+        Name = "DB Server 1"
+    }
+}
---
+++ public.tf
@@ -0,0 +1,66 @@
+/*
+  Web Servers
+*/
+resource "aws_security_group" "web" {
+    name = "vpc_web"
+    description = "Allow incoming HTTP connections."
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress { # SQL Server
+        from_port = 1433
+        to_port = 1433
+        protocol = "tcp"
+        cidr_blocks = ["${var.private_subnet_cidr}"]
+    }
+    egress { # MySQL
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        cidr_blocks = ["${var.private_subnet_cidr}"]
+    }
+
+    vpc_id = "${aws_vpc.default.id}"
+
+    tags {
+        Name = "WebServerSG"
+    }
+}
+
+resource "aws_instance" "web-1" {
+    ami = "${lookup(var.amis, var.aws_region)}"
+    availability_zone = "eu-west-1a"
+    instance_type = "m1.small"
+    key_name = "${var.aws_key_name}"
+    security_groups = ["${aws_security_group.web.id}"]
+    subnet_id = "${aws_subnet.eu-west-1a-public.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+
+
+    tags {
+        Name = "Web Server 1"
+    }
+}
+
+resource "aws_eip" "web-1" {
+    instance = "${aws_instance.web-1.id}"
+    vpc = true
+}
